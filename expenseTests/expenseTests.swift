28611d0871d1890c57de9215c838c883cb956fec
Robin Liao <hi@robinliao.com>
2025-08-07 17:36:45 -0700

    Initial Commit

---
+++ expenseTests/expenseTests.swift
@@ -0,0 +1,17 @@
+//
+//  expenseTests.swift
+//  expenseTests
+//
+//  Created by Robin Liao on 8/7/25.
+//
+
+import Testing
+@testable import expense
+
+struct expenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
